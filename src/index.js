a574fd1bc62ad7e5b94a803b4b4cfdc66ab78b6c
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2021-02-15 09:20:09 -0500

    add content to approvalcard

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45pm" 
-        content="Good job" 
-        avatar={faker.image.cats()}
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45pm" 
+          content="Good job" 
+          avatar={faker.image.cats()}
         />
-      <CommentDetail 
-        author ="Alex" 
-        timeAgo="Today at 2:00am" 
-        content="nice" 
-        avatar={faker.image.animals()}
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author ="Alex" 
+          timeAgo="Today at 2:00am" 
+          content="nice" 
+          avatar={faker.image.animals()}
         />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 5:00pm" 
-        content="hello there" 
-        avatar={faker.image.sports()}
-      />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 5:00pm" 
+          content="hello there" 
+          avatar={faker.image.sports()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
