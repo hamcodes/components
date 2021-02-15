bdb1008fc9f989f75b5907e6573c990f28211040
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2021-02-15 08:46:33 -0500

    use props in commmentdetail

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author ="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45pm" 
+        content="Good job" 
+        image={faker.image.cats()}
+        />
+      <CommentDetail 
+        author ="Alex" 
+        timeAgo="Today at 2:00am" 
+        content="nice" 
+        image={faker.image.animals()}
+        />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Yesterday at 5:00pm" 
+        content="hello there" 
+        image={faker.image.sports()}
+      />
     </div>
   );
 };
